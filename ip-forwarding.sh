38c094900f97b7e4f109d0bd01e9cbe894e0a568
H. Nikolaus Schaller <hns@goldelico.com>
2016-06-06 11:05:14 +0200

    simplify ip-forwarding for 10.11
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- ip-forwarding.sh
+++ ip-forwarding.sh
@@ -1,4 +1,5 @@
-WLAN=en1
 sudo sysctl -w net.inet.ip.forwarding=1
-sudo natd -interface $WLAN
-sudo ipfw add divert natd ip from any to any via $WLAN
+# no longer needed on OS X 10.11
+# WLAN=en1
+# sudo natd -interface $WLAN
+# sudo ipfw add divert natd ip from any to any via $WLAN
