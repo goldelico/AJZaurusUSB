7e93eb7db1bad501102f8d933a3313df7ba43759
H. Nikolaus Schaller <hns@goldelico.com>
2017-04-23 13:49:00 +0200

    ip-forwarding: give utun0 (Tunnelblick) priority and improve code to search the highest priority network
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- ip-forwarding.sh
+++ ip-forwarding.sh
@@ -2,40 +2,51 @@
 
 # based on
 # http://apple.stackexchange.com/questions/228936/using-server-5-0-15-to-share-internet-without-internet-sharing
+# https://superuser.com/questions/931827/sharing-openvpn-on-mac-os-x-yosemite
 
 DEVICE=192.168.0.202	# Letux device
 
-# found at https://apple.stackexchange.com/questions/191879/how-to-find-the-currently-connected-network-service-from-the-command-line
+if ifconfig "utun0" >/dev/null
+then # VPN hides highest priority active network
+	CURRENT="tunnelblick"
+	INTERFACE="utun0"
 
-SERVICES=$(networksetup -listnetworkserviceorder | grep 'Hardware Port')
-
-while read LINE
-do
-	NAME=$(echo $LINE | awk -F  "(, )|(: )|[)]" '{print $2}')
-	DEV=$(echo $LINE | awk -F  "(, )|(: )|[)]" '{print $4}')
-	#echo "Current service: $NAME, $DEV, $CURRENT"
-	if [ -n "$DEV" ]
-	then
-		ifconfig "$DEV" 2>/dev/null | grep 'status: active' > /dev/null 2>&1
-	rc="$?"
-	if [ "$rc" -eq 0 ]
-	then
-		CURRENT="$NAME"
-		INTERFACE="$DEV"
-	fi
-	fi
-done <<< "$(echo "$SERVICES")"
-
-if [ -n $CURRENT ]
-then
-	echo Sharing over: $CURRENT $INTERFACE
 else
-	>&2 echo "Could not find current service"
-	exit 1
+
+# locate highest priority active service
+# found at https://apple.stackexchange.com/questions/191879/how-to-find-the-currently-connected-network-service-from-the-command-line
+# and improved...
+
+	while read LINE
+	do
+		NAME=$(echo $LINE | awk -F  "(, )|(: )|[)]" '{print $2}')
+		DEV=$(echo $LINE | awk -F  "(, )|(: )|[)]" '{print $4}')
+		# echo "Current service: $NAME, $DEV, $CURRENT, $INTERFACE"
+		if [ "$DEV" ]
+		then
+			if ifconfig "$DEV" 2>/dev/null | grep -q 'status: active'
+			then
+				CURRENT="$NAME"
+				INTERFACE="$DEV"
+				break 2	# take first we find
+			fi
+		fi
+	done < <(networksetup -listnetworkserviceorder | grep 'Hardware Port')
+
+	if [ -n $CURRENT ]
+	then
+		echo Sharing over: $CURRENT $INTERFACE
+	else
+		>&2 echo "Could not find current service"
+		exit 1
+	fi
+
 fi
 
 sudo pfctl -F nat
 
+#echo "nat log on $INTERFACE from $DEVICE to any -> ($INTERFACE)
+#nat log on en0 from $DEVICE to any -> (en0)" | sudo pfctl -N -f - -e
 echo "nat log on $INTERFACE from $DEVICE to any -> ($INTERFACE)" | sudo pfctl -N -f - -e
 sudo pfctl -a '*' -s nat
 
