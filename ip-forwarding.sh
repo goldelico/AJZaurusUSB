2150eec5199a5f7fdfa7a67b0c0aa07df8678241
H. Nikolaus Schaller <hns@goldelico.com>
2017-04-23 12:45:08 +0200

    ip-forwarding: serarch for first active interface like shown in System Settings/Network
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- ip-forwarding.sh
+++ ip-forwarding.sh
@@ -1,17 +1,42 @@
-#! /bin/sh
+#! /bin/bash
 
 # based on
 # http://apple.stackexchange.com/questions/228936/using-server-5-0-15-to-share-internet-without-internet-sharing
 
-### besser: aus networksetup -listallnetworkservices heraussuchen
+DEVICE=192.168.0.202	# Letux device
 
-internet=en1
-[ "$1" ] && internet=$1
-device=192.168.0.202	# Letux device
+# found at https://apple.stackexchange.com/questions/191879/how-to-find-the-currently-connected-network-service-from-the-command-line
+
+SERVICES=$(networksetup -listnetworkserviceorder | grep 'Hardware Port')
+
+while read LINE
+do
+	NAME=$(echo $LINE | awk -F  "(, )|(: )|[)]" '{print $2}')
+	DEV=$(echo $LINE | awk -F  "(, )|(: )|[)]" '{print $4}')
+	#echo "Current service: $NAME, $DEV, $CURRENT"
+	if [ -n "$DEV" ]
+	then
+		ifconfig "$DEV" 2>/dev/null | grep 'status: active' > /dev/null 2>&1
+	rc="$?"
+	if [ "$rc" -eq 0 ]
+	then
+		CURRENT="$NAME"
+		INTERFACE="$DEV"
+	fi
+	fi
+done <<< "$(echo "$SERVICES")"
+
+if [ -n $CURRENT ]
+then
+	echo Sharing over: $CURRENT $INTERFACE
+else
+	>&2 echo "Could not find current service"
+	exit 1
+fi
 
 sudo pfctl -F nat
 
-echo "nat log on $internet from $device to any -> ($internet)" | sudo pfctl -N -f - -e
+echo "nat log on $INTERFACE from $DEVICE to any -> ($INTERFACE)" | sudo pfctl -N -f - -e
 sudo pfctl -a '*' -s nat
 
 sudo sysctl -w net.inet.ip.forwarding=1
@@ -21,4 +46,4 @@ sudo sysctl -w net.inet.ip.fw.enable=1
 ## logging
 # ifconfig pflog1 create
 # sudo tcpdump -n -e -ttt -i pflog1
-# sudo tcpdump -n -e -ttt -i $internet
+# sudo tcpdump -n -e -ttt -i $INTERFACE
